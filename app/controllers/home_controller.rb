a8623750c10527178f0910e2736ab75d2c97841e
LOAP <program@livingonaprayer.org>
2012-11-19 16:35:41 -0500

    Added FrontPage blog

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
     @users = User.all
+    @posts = Post.paginate(:order => "created_at DESC", :page => params[:page], :per_page => 4)
   end
 end
