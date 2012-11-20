8178d753e09db12ce8688d09f2367db84465351e
LOAP <program@livingonaprayer.org>
2012-11-19 23:24:26 -0500

    commit before bed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
     @users = User.all
-    @posts = Post.paginate(:order => "created_at DESC", :page => params[:page], :per_page => 4)
+    @profile = Profile.all
+    @posts = Post.paginate(:order => "created_at DESC", :page => params[:page], :per_page => 50)
   end
 end
